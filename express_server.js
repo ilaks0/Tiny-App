df32e14c1570575dc37d7cd8347c1f9abff1bbbb
ilaksono <ian.laksono@gmail.com>
2020-09-28 18:14:49 +0000

    added url database get request

--- express_server.js
+++ express_server.js
@@ -1,15 +1,29 @@
 const express = require('express');
 const app = express();
-const server = app.listen(8080, null);
-const login = require('login');
+const port  = 8080;
+// const login = require('login');
+
+const urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
+
+
 app.get('/', (req, res) => {
-  res.send('');
-  if (login) {
-    app.route('/urls', (req, res) => {
-      res.send('');
-    })
-  } else
-    app.route('/urls', (req, res) => {
-      res.send('');
-    })
+  res.send('Hello!');
+  // if (login) {
+  //   app.route('/urls', (req, res) => {
+  //     res.send('');
+  //   })
+  // } else
+  // app.route('/urls', (req, res) => {
+  //   res.send('');
+  // })
 });
+
+app.get('/urls.json', (req,res) => {
+  res.json(urlDatabase);
+})
+
+const server = app.listen(port, () => console.log('listening ', port));
