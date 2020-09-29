38ca18bedc605f8c2a37ef0bba8fc0f2ad9b10f0
ilaksono <ian.laksono@gmail.com>
2020-09-29 18:47:56 +0000

    added users account obj

--- express_server.js
+++ express_server.js
@@ -2,7 +2,6 @@ const express = require('express');
 const app = express();
 const port = 8080;
 const cookieParser = require('cookie-parser');
-
 const bodyParser = require("body-parser");
 const generateRandomString = () => Math.random().toString(36).substring(2, 8);
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -11,8 +10,21 @@ const urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
+const users = {
+  "userRandomID": {
+    id: "userRandomID",
+    email: "user@example.com",
+    password: "purple-monkey-dinosaur"
+  },
+  "user2RandomID": {
+    id: "user2RandomID",
+    email: "user2@example.com",
+    password: "dishwasher-funk"
+  }
+}
 
 app.set("view engine", "ejs");
+
 app.post('/login', (req, res) => {
   res.cookie('username', req.body.username);
   res.redirect('/urls/');
@@ -25,7 +37,7 @@ app.get('/urls', (req, res) => {
   let username = req.cookies.username || '';
   const templateVars = {
     urls: urlDatabase,
-    username: username
+    users: users
   }
   res.render('urls_index', templateVars);
 })
@@ -33,7 +45,7 @@ app.get('/urls/new', (req, res) => {
   let username = req.cookies.username || '';
   const templateVars = {
     urls: urlDatabase,
-    username: username
+    users: users
   }
   res.render('urls_new', templateVars);
 })
@@ -50,10 +62,19 @@ app.post('/urls/:shortURL/delete', (req, res) => {
 app.get('/register', (req, res) => {
   let username = req.cookies.username || '';
   const templateVars = {
-    username: username
+    users: users
   }
   res.render('user_registration', templateVars);
-})
+});
+
+app.post('/register', (req, res) => {
+  let randId = generateRandomString();
+  const obj = { id: randId, email: req.body.email, password: req.body.password };
+  users[obj.id] = obj;
+  res.cookie('user_id', obj);
+  res.redirect('/urls');
+});
+
 app.post('/urls/:shortURL', (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect(300, `/urls/`);
