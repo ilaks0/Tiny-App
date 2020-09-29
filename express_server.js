54f495b3a9a985ecf7c1a48563981778f0c90fb9
ilaksono <ian.laksono@gmail.com>
2020-09-29 22:03:37 +0000

    added login page and cookie id

--- express_server.js
+++ express_server.js
@@ -22,63 +22,100 @@ const users = {
     password: "dishwasher-funk"
   }
 }
-
+const idHelper = idCan => {
+  for (let user in users) {
+    if (idCan === user)
+      return true;
+  }
+  return false;
+};
 app.set("view engine", "ejs");
 
+app.get('/login', (req, res) => {
+  let userId;
+  idHelper(req.cookies['user_id']) ? userId = users[req.cookies['user_id']].email : userId = '';
+  const templateVars = {
+    user: userId
+  }
+  res.render('user_login', templateVars);
+});
+
 app.post('/login', (req, res) => {
-  res.cookie('username', req.body.username);
-  res.redirect('/urls/');
+  for (let user in users) {
+    if (req.body.email === users[user].email && req.body.password === users[user].password) {
+      res.cookie('user_id', user);
+      res.redirect(200, '/urls');
+      return;
+    }
+  }
+  res.redirect(403, '/login');
 });
 
 app.get('/', (req, res) => {
   res.send('Hello!');
 });
+
 app.get('/urls', (req, res) => {
-  let username = req.cookies.username || '';
+  let userId;
+  idHelper(req.cookies['user_id']) ? userId = users[req.cookies['user_id']].email : userId = '';
   const templateVars = {
     urls: urlDatabase,
-    users: users
+    user: userId
   }
   res.render('urls_index', templateVars);
-})
+});
+
 app.get('/urls/new', (req, res) => {
-  let username = req.cookies.username || '';
+  let userId;
+  idHelper(req.cookies['user_id']) ? userId = users[req.cookies['user_id']].email : userId = '';
   const templateVars = {
     urls: urlDatabase,
-    users: users
+    user: userId
   }
   res.render('urls_new', templateVars);
-})
+});
+
 app.post('/urls', (req, res) => {
   let newStr = generateRandomString();
   urlDatabase[newStr] = req.body.longURL;
   res.redirect(300, `/urls/${newStr}`);
-})
+});
 
 app.post('/urls/:shortURL/delete', (req, res) => {
   delete urlDatabase[req.params.shortURL];
   res.redirect(200, `/urls/`);
 });
 app.get('/register', (req, res) => {
-  let username = req.cookies.username || '';
+  let userId;
+  idHelper(req.cookies['user_id']) ? userId = users[req.cookies['user_id']].email : userId = '';
   const templateVars = {
-    users: users
+    user: userId
   }
   res.render('user_registration', templateVars);
 });
 
 app.post('/register', (req, res) => {
+  if (!(req.body.email) || !(req.body.password)) {
+    res.redirect(400, '/register');
+    return;
+  }
+  for (let user in users) {
+    if (req.body.email === users[user].email) {
+      res.redirect(400, '/register');
+      return;
+    }
+  }
   let randId = generateRandomString();
   const obj = { id: randId, email: req.body.email, password: req.body.password };
   users[obj.id] = obj;
-  res.cookie('user_id', obj);
-  res.redirect('/urls');
+  res.redirect(200, '/urls');
 });
 
 app.post('/urls/:shortURL', (req, res) => {
   urlDatabase[req.params.shortURL] = req.body.longURL;
   res.redirect(300, `/urls/`);
-})
+});
+
 app.get('/u/:shortURL', (req, res) => {
   if (req.params.shortURL === 'undefined') {
     res.send('404 Page Not Found');
@@ -89,25 +126,26 @@ app.get('/u/:shortURL', (req, res) => {
     const longURL = urlDatabase[req.params.shortURL];
     res.redirect(300, `${longURL}`);
   }
-})
+});
 
 app.get('/urls.json', (req, res) => {
   res.json(urlDatabase);
-})
+});
+
 app.post('/logout', (req, res) => {
-  res.clearCookie('username');
+  res.clearCookie('user_id');
   res.redirect(200, '/urls');
-})
+});
 
 app.get('/urls/:shortURL', (req, res) => {
-  let username = req.cookies.username || '';
+  let userId;
+  idHelper(req.cookies['user_id']) ? userId = users[req.cookies['user_id']].email : userId = '';
   const templateVars = {
     shortURL: req.params.shortURL,
     longURL: urlDatabase[req.params.shortURL],
-    username: username
+    user: userId
   }
   res.render('urls_show', templateVars)
-})
+});
 
-app.use(bodyParser.urlencoded({ extended: true }));
 const server = app.listen(port, () => console.log('listening ', port));
